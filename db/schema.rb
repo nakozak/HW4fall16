1a745e05ec16b31f1205dbe3117108ffdf4038d5
nakozak <nicholas-kozak@uiowa.edu>
2016-09-29 15:47:45 +0000

    Fixed User Ids be redoing the db schema and the users controller. Now onto fixing sessions...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926163646) do
+ActiveRecord::Schema.define(version: 20160929153701) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -23,12 +23,11 @@ ActiveRecord::Schema.define(version: 20160926163646) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_id"
-    t.string "email"
-    t.string "session_token"
+    t.string   "user_id"
+    t.string   "email"
+    t.string   "session_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    
   end
 
 end
